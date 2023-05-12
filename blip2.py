c96ed394d66199ca4a1974fba5a54305a2908d8a
Taise228 <yamamototaisei534@gmail.com>
2023-05-12 15:25:10 +0900

    model download

--- blip2.py
+++ blip2.py
@@ -7,7 +7,15 @@ from lavis.models import load_model_and_preprocess
 def main():
     url = "http://images.cocodataset.org/val2017/000000039769.jpg"
     raw_image = Image.open(requests.get(url, stream=True).raw)
+    
+    model, vis_processors, _ = load_model_and_preprocess(name="blip2_t5", model_type="pretrain_flant5xxl", is_eval=True)
+    print('model downloaded')
 
+    image = vis_processors['eval'](raw_image).unsqueeze(0)
+
+    print(type(image))
+
+    model.generate({'image': image, 'prompt': 'Question: what are in the picture? Answer:'})
 
 
 
